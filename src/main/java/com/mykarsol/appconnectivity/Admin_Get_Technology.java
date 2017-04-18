75fb8964957bc2823bd34206ec9230e8c433be22
Shivam-sp <shivam2296.ps@gmail.com>
2017-04-18 18:37:34 +0530

    Update Admin_Get_Technology.java

--- src/main/java/com/mykarsol/appconnectivity/Admin_Get_Technology.java
+++ src/main/java/com/mykarsol/appconnectivity/Admin_Get_Technology.java
@@ -61,9 +61,7 @@ public class Admin_Get_Technology extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     static final String JDBC_DRIVER="com.mysql.jdbc.Driver";  
-      static final String DB_URL="jdbc:mysql://fock27f4fz512g18:puc2ocwfqnvrxed1@vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uioyzcynsq8h3m7h
-
-";
+      static final String DB_URL="jdbc:mysql://fock27f4fz512g18:puc2ocwfqnvrxed1@vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uioyzcynsq8h3m7h";
       //  Database credentials
       static final String USER = "fock27f4fz512g18";
       static final String PASS = "puc2ocwfqnvrxed1";
